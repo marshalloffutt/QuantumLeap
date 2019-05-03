991389a9991105d880fd21183ac5ac9f722a204d
unknown <marshalloffutt@gmail.com>
2019-05-02 19:41:54 -0500

    add leaper

---
+++ QuantumLeap/Data/LeaperRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using QuantumLeap.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumLeap.Data
+{
+    public class LeaperRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=QuantumLeap;Trusted_Connection=True;";
+
+        public Leaper AddLeaper(string name, int age)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var insertQuery = @"
+                    Insert Into [dbo].[Leapers]([Name],[Age]])
+                    Output inserted.*
+                    Values (@name, @age)";
+
+                var parameters = new
+                {
+                    Name = name,
+                    Age = age,
+                };
+
+                var newLeaper = db.QueryFirstOrDefault<Leaper>(insertQuery, parameters);
+
+                if (newLeaper != null)
+                {
+                    return newLeaper;
+                }
+            }
+            throw new Exception("Could not create leaper");
+        }
+    }
+}
---
+++ QuantumLeap/Models/Leaper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumLeap.Models
+{
+    public class Leaper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Leaper(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
