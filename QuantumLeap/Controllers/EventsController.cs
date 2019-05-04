99a1509f5bde19c364c71a6d0a7f3a89615c57cc
unknown <marshalloffutt@gmail.com>
2019-05-04 11:58:41 -0500

    Add and get events

---
+++ QuantumLeap/Controllers/EventsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuantumLeap.Data;
+using QuantumLeap.Models;
+
+namespace QuantumLeap.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        readonly EventRepository _eventRepository;
+
+        public EventsController()
+        {
+            _eventRepository = new EventRepository();
+        }
+
+        [HttpGet]
+        public ActionResult GetAllEvents()
+        {
+            var events = _eventRepository.GetAllEvents();
+
+            return Ok(events);
+        }
+
+        [HttpPost("add")]
+        public ActionResult AddEvent(CreateEventRequest createRequest)
+        {
+            var newEvent = _eventRepository.AddEvent(
+                createRequest.Description,
+                createRequest.Location,
+                createRequest.Year,
+                createRequest.IsCorrect);
+
+            return Created($"/api/event/{newEvent.Id}", newEvent);
+        }
+    }
+}
